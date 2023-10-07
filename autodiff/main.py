a56856512b016f138e0d5c258e798d6307f9031a
Tobias Kadenbach <tobias.kadenbach@spork.codes>
2023-10-07 14:28:39 +0200

    feat: finish first version

--- autodiff/main.py
+++ autodiff/main.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 import subprocess
+import json
 
-last_tag_cmd = subprocess.run(['git', 'diff', '--name-only', 'HEAD', 'HEAD~1'], stdout=subprocess.PIPE)
-last_tag = last_tag_cmd.stdout.decode("utf-8")
-last_tag = last_tag.replace("\n", "")
-print(last_tag)
+diff_cmd = subprocess.run(['git', 'diff', '--name-status', 'HEAD', 'HEAD~1'], stdout=subprocess.PIPE)
+diff_files = diff_cmd.stdout.decode("utf-8")
+files = diff_files.split("\n")
+files.pop()
+result_tuples = [(s.split('\t')[0], s.split('\t')[1]) for s in files]
+
+
+print("files=" + json.dumps(result_tuples))
